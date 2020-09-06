51d07417101dd7f663b6794a04e3b5403dda21ed
rianne <2025798@talnet.nl>
2020-09-06 13:12:59 +0200

    git commit signup.php

---
+++ signup.php
@@ -0,0 +1,49 @@
+<?php
+//new user form
+ ?>
+
+<form action="signup.php" method="POST">
+  <p>
+    <label> Username </label>
+      <input id="username" value="" name="username" type="text" required="required">
+    <br>
+  </p>
+
+    <p>
+    <label> Tussenvoegsel </label>
+      <input id="tussenvoegsel" value="" name="tussenvoegsel" type="text">
+    <br>
+  </p>
+
+  <p>
+    <label> Achternaam </label>
+      <input id="achternaam" value="" name="achternaam" type="text" required="required">
+    <br>
+  </p>
+
+  <p>
+    <label> Email </label>
+      <input id="email" value="" name="email" type="text" required="required">
+    <br>
+  </p>
+
+  <p>
+    <label> Gebruikersnaam </label>
+      <input id="gebruikersnaam" value="" name="gebruikersnaam" type="text" required="required">
+    <br>
+  </p>
+
+    <p>
+    <label> wachtwoord </label>
+      <input id="wachtwoord" value="" name="wachtwoord" type="text" required="required">
+    <br>
+  </p>
+
+  <p>
+    <label> herhaal wachtwoord </label>
+      <input id="hwachtwoord" value="" name="hwachtwoord" type="text" required="required">
+    <br>
+  </p>
+<button type="submit"><span>Word Gebruiker</span></button>
+
+</form>
