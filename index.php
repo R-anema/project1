ef678d309a1505a7f724402d875dc12ecdb633ed
rianne <2025798@talnet.nl>
2020-09-06 13:12:36 +0200

    git commit index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+//login form
+//password, login knop, link met account aanmaken, lost password link
+
+ ?>
+
+<form action="index.php" method="POST">
+  <p>
+    <label> Username </label>
+      <input id="username" value="" name="username" type="text" required="required">
+    <br>
+  </p>
+
+  <p>
+    <label> Password </label>
+      <input id="password" value="" name="password" type="text" required="required">
+    <br>
+  </p>
+
+<button type="submit"><span>Login</span></button>
+<br>
+<button type="submit"><span>Create Account</span></button>
+<button type="submit"><span>Lost Password Link</span></button>
+
+</form>
