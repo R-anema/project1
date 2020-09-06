fadf89f46e01880b22cef1ae4eabdfb2a1e990d5
rianne <2025798@talnet.nl>
2020-09-06 13:13:48 +0200

    git commit lostpsw.php

---
+++ lostpsw.php
@@ -0,0 +1,14 @@
+<?php
+//lost password form
+ ?>
+
+<form action="signup.php" method="POST">
+  <p>
+    <label> Email </label>
+      <input id="email" value="" name="email" type="text" required="required">
+    <br>
+  </p>
+
+<button type="submit"><span>Lost password request</span></button>
+
+</form>
