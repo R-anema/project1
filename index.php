3129947fc0ce735557233c998cd95aef6605e38d
rianne <2025798@talnet.nl>
2020-09-11 12:00:04 +0200

    git commit index.php

--- index.php
+++ index.php
@@ -4,18 +4,20 @@
 
  ?>
 
+<!DOCTYPE html>
+<html>
 <form action="index.php" method="POST">
-  <p>
-    <label> Username </label>
-      <input id="username" value="" name="username" type="text" required="required">
-    <br>
-  </p>
+	<p>
+		<label> Username </label>
+			<input id="username" value="" name="username" type="text" required="required">
+		<br>
+	</p>
 
-  <p>
-    <label> Password </label>
-      <input id="password" value="" name="password" type="text" required="required">
-    <br>
-  </p>
+	<p>
+		<label> Password </label>
+			<input id="password" value="" name="password" type="text" required="required">
+		<br>
+	</p>
 
 <button type="submit"><span>Login</span></button>
 <br>
@@ -23,3 +25,5 @@
 <button type="submit"><span>Lost Password Link</span></button>
 
 </form>
+
+</html>
