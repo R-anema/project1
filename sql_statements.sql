aeb9bfbdf36ac6705f092125ba7ebf080c159d53
rianne <2025798@talnet.nl>
2020-09-16 14:37:12 +0200

    git commit sql_statement.sql

--- sql_statements.sql
+++ sql_statements.sql
@@ -1,15 +1,18 @@
+Create database persoon1;
+Use persoon1;
+
 CREATE TABLE Account (
     ID int not null,
-	email varchar(255) UNIQUE,
-	password varchar(255),
+	email varchar(255) UNIQUE not null,
+	password varchar(255) not null,
 	PRIMARY KEY(ID)
 );
 
 CREATE TABLE Persoon (
     account_id int not null,
-	naam varchar(255),
+	naam varchar(255) not null,
     tussenvoegsel varchar (255),
-	achternaam varchar(255),
-    gebruikersnaam varchar(255),
+	achternaam varchar(255) not null,
+    gebruikersnaam varchar(255) not null,
     FOREIGN KEY(account_id) REFERENCES Account(ID)
 );
