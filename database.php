09e64a42f7c5d8b2f37e6ee43a9ca23b0e5b655d
rianne <2025798@talnet.nl>
2020-10-05 12:44:48 +0200

    git commit database.php

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-//class database geindent volgens voorbeel PPH website
+//class database geindent volgens voorbeel PHP website
 class database{
 
 	private $host;
@@ -15,16 +15,27 @@ class database{
 		$this->$password = $password;
 		$this->$database = $database;
 		$this->$charset = $charset;
-		$this->$db = $db;
+		$this->$db = $db; // weet niet of die moet blijven.
 
 	try {
-		//indented with tab
-		$this->host = 'localhost';
-	}catch(PDOexceptionTBa){
-		rollbacktba();
+		//indented with tab, beginnen aan connectie Database
+		$dsn = "mysql:host-$this->$host;dbname-$this->$database;charset-$this->charset";
+		$this->db = new PDO($dsn, $this->$username, $this->$password);
+		echo 'Succesfully connected';
+	} //error opvangen
+	catch(PDOexception $e){
+		$e->getMessage();
+		exit('an error occured');
 	}
+	//account aanmaken moet in signup worden aangeroepen, en insert into ook gebruikt worden. TBA Singup checken of hij word aangeroepen, en afstemmen met.
+	public function addAcount($voornaam, $tussenvoegsel, $achternaam, $email, $username, $password){ //error hierzo
+		try{
+			$this->db = new PDO($dsn, $this->$username, $this->$password); //nog bijwerken voor deze functie.
+
+		}
 
 	}
+
 };
 
 ?>
