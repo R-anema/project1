fca9c8bda04e41c43e647e0ea385970abf00a1c8
rianne <2025798@talnet.nl>
2020-10-25 14:59:13 +0100

    git commit lostpsw.php

--- lostpsw.php
+++ lostpsw.php
@@ -6,17 +6,12 @@
 <!DOCTYPE html>
 <html>
   <form action="lostpsw.php" method="POST">
-    <p>
+
       <label> Email </label>
         <input id="email" value="" name="email" type="text" required="required">
-      <br>
-    </p>
 
-    <p>
       <label> Submit </label>
           <input type= "submit" value ="Submit"><span>Verstuur Aanvraag</span>
-      <br>
-    </p>
 
   </form>
 </html>
