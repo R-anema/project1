5152cd5837b1db425973d92b28cd5e8723d9a72a
rianne <2025798@talnet.nl>
2020-10-25 14:58:49 +0100

    git commit index.php

--- index.php
+++ index.php
@@ -1,21 +1,35 @@
 <?php
-//login form
-//password, login knop, link met account aanmaken, lost password link
+//login form, password, login knop, link met account aanmaken, lost password link.
+// check the signup.php file for explanation on code
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit']) && !empty($_POST['submit'])){
+    $fields = ['uname', 'pwd'];
+
+    $obj2 = new Helper();
+    $fields_validated = $obj2->field_validation($fields);
+
+    if($fields_validated){
+        $uname = trim($_POST['uname']);
+        $password = trim($_POST['pwd']);
+        
+        $obj = new database('localhost', 'root', '', 'project1', 'utf8');
+
+        // user redirected to welcome page in case of succesful login.
+        // unsuccessfull login results in an error message (string)
+        $loginError = $obj->login($uname, $password);
+    }
+}
 
  ?>
 
 <!DOCTYPE html>
 <html>
 <form action="index.php" method="POST">
-	<p>
+
 		<label> Username </label>
 			<input id="username" value="" name="username" type="text" required="required"><br>
-	</p>
 
-	<p>
 		<label> Password </label>
 			<input id="password" value="" name="password" type="text" required="required"><br>
-	</p>
 
 <input type="sumbit" name="login" value="login"><span>Login</span></button>
 <a href="lostpsw.php"> Watchwoord veloren?</a>
