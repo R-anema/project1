b9e0b0ca7db594c2ac7a66639a0a171e69d23936
rianne <2025798@talnet.nl>
2020-10-25 14:57:10 +0100

    git commit database.php

--- database.php
+++ database.php
@@ -2,12 +2,15 @@
 //class database geindent volgens voorbeel PHP website
 class database{
 
+	//$db = mysql_connect('localhost', 'root', '', 'signup');
+
 	private $host;
 	private $username;
 	private $password;
 	private $database;
 	private $charset;
 	private $db;
+	private $database_name;
 
 	public function __construct($host, $username, $password, $database, $charset, $db){
 		$this->$host = $host;
@@ -26,15 +29,44 @@ class database{
 	catch(PDOexception $e){
 		$e->getMessage();
 		exit('an error occured');
-	}
-	//account aanmaken moet in signup worden aangeroepen, en insert into ook gebruikt worden. TBA Singup checken of hij word aangeroepen, en afstemmen met.
-	public function addAcount($voornaam, $tussenvoegsel, $achternaam, $email, $username, $password){ //error hierzo
-		try{
-			$this->db = new PDO($dsn, $this->$username, $this->$password); //nog bijwerken voor deze functie.
+	} if (isset($_POST ['signup.php'])) {
+		//hier alle input values van de form. Dit misschien naar signup sturen.
 
+	}	catch(PDOException $e){
+		// die and exit are equivalent
+		// exit-> Output a message and terminate the current script
+		die("Unable to connect: " . $e->getMessage());
+	}
+
+
+	//if signup word aangeroepen: account aanmaken moet in signup en insert into ook gebruiken. TBA Singup checken of hij word aangeroepen, en afstemmen met.
+	public function addAcount($voornaam, $tussenvoegsel, $achternaam, $email, $username, $password){
+		try{
+			$this->db->beginTransaction();
+			echo "Begin Transactie pls";
+			
+			//TODO: wachtwoord en username ophalen/registeren. 
+			$this->db = new PDO($dsn, $this->$username, $this->$password);
 		}
 
 	}
+
+	private function is_admin($username){
+        $sql = "SELECT type_id FROM account WHERE username = :username";
+
+        $stmt = $this->dbh->prepare($sql);
+        $stmt->execute(['username'=>$username]);
+
+        // result is an associative array (key-value pair)
+        $result = $stmt->fetch();
+        
+        if($result['type_id'] == self::ADMIN){
+            return true;
+        }
+
+        // user is not admin
+        return false;
+    }
 
 };
 
